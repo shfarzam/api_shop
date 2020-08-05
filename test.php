a333bebe137d4d617fa1ce7abad714c0fda2eba5
Shahriar Farzam <sh.farzam@hotmail.com>
2020-08-05 22:19:56 +0200

    Update Test file

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+if(isset($_GET['name'])) {
+    print_r(json_encode([ "GET parameter" => $_GET['name']]));
+}
+if(isset($_POST['name'])) {
+    print_r(json_encode([ "POST parameter" => $_POST['name']]));
+}
